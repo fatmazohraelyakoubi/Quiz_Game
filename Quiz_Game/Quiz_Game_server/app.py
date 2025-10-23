ca8b481e5ed421d439f5af90aec1882145c7e0f0
fatmazohraelyakoubi <fatmazohrayakoubi6@gmail.com>
2025-10-23 18:28:04 +0100

    Updated Quiz styling and fixed UI

--- Quiz_Game/Quiz_Game_server/app.py
+++ Quiz_Game/Quiz_Game_server/app.py
@@ -1,20 +1,33 @@
 from flask import Flask,request,jsonify
 from flask_cors import CORS
+import json,random
+
 
 app=Flask(__name__)
-CORS(app)
+CORS(app,origin=["http://localhost:3000"])
+
+  
+with open('questions.json') as f:
+    questions=json.load(f)
+
+@app.route('/question',methods=["GET"])
+def get_question():
+    question=random.choice(questions)
+    return jsonify(question)
 
 
 @app.route('/check-answer', methods=['POST'])
-
 def check_answer():
     data=request.get_json()
-    answer=data.get('answer','')
+    answer=data.get('answer','').stripe()
+    correct=data.get('correct_answer','').stripe()
 
-    if answer == "cooperative":
-          return jsonify({"result": "correct"})  
+    if answer == correct:
+           return jsonify({"result": "correct"})  
     else:
            return jsonify({"result": "wrong"}) 
+
+
 
 
 
